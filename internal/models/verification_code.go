ae82689940ec558e1274abc0baa47f89d04e876b
kurabier <kapelkalina@mail.ru>
2026-05-06 23:05:51 +0300

    Также исправлено на uint, добавлен Gorm и поле User с каскадным удалением

--- internal/models/verification_code.go
+++ internal/models/verification_code.go
@@ -10,11 +10,14 @@ const (
 )
 
 type VerificationCode struct {
-	ID        int                  `json:"id"`
-	UserID    int                  `json:"user_id"`
-	Code      string               `json:"code"`
-	Type      VerificationCodeType `json:"type"`
-	ExpiresAt time.Time            `json:"expires_at"`
-	Used      bool                 `json:"used"`
-	CreatedAt time.Time            `json:"created_at"`
+	ID        uint                 `gorm:"primaryKey;autoIncrement" json:"id"`
+	UserID    uint                 `gorm:"not null;index"           json:"user_id"`
+	Code      string               `gorm:"not null"                 json:"code"`
+	Type      VerificationCodeType `gorm:"not null"                 json:"type"`
+	ExpiresAt time.Time            `gorm:"not null"                 json:"expires_at"`
+	Used      bool                 `gorm:"default:false"            json:"used"`
+	CreatedAt time.Time            `gorm:"autoCreateTime"           json:"created_at"`
+
+	// cвязь с таблицей users, а при удалении пользователя все его коды удаляются автоматически, дабы не создавать мусор в БД
+	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"-"`
 }
