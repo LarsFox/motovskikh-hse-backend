02c565a81218f7d20631a0cecb1dbc9e2ad33d8b
kurabier <kapelkalina@mail.ru>
2026-05-08 23:17:45 +0300

    manager: добавитла auth и token сервисы

--- manager/manager.go
+++ manager/manager.go
@@ -1,19 +1,46 @@
 package manager
 
+import (
+	"github.com/LarsFox/motovskikh-hse-backend/internal/repository"
+	"github.com/LarsFox/motovskikh-hse-backend/internal/services"
+)
+
 type db interface {
 	Stub() bool
 }
 
 type Manager struct {
-	db db
+	db           db
+	authService  *services.AuthService
+	tokenService *services.TokenService
 }
 
-func New(db db) *Manager {
+func New(
+	db db,
+	userRepo repository.UserRepository,
+	codeRepo repository.VerificationCodeRepository,
+	refreshRepo repository.RefreshTokenRepository,
+	emailSender services.EmailSender,
+	jwtSecret string,
+) *Manager {
+	tokenService := services.NewTokenService(jwtSecret, refreshRepo)
+	authService := services.NewAuthService(userRepo, codeRepo, emailSender)
+
 	return &Manager{
-		db: db,
+		db:           db,
+		authService:  authService,
+		tokenService: tokenService,
 	}
 }
 
 func (m *Manager) Stub() bool {
 	return m.db.Stub()
+}
+
+func (m *Manager) Auth() *services.AuthService {
+	return m.authService
+}
+
+func (m *Manager) Token() *services.TokenService {
+	return m.tokenService
 }
