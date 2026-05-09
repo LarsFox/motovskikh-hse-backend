bd2993e88f39b7bac5b53d414b503dcafe34fbc2
kurabier <kapelkalina@mail.ru>
2026-05-10 02:11:39 +0300

    migrations: исправление int на uint в БД  для корректной работы с GORM

--- migrations/002_create_verification_codes.sql
+++ migrations/002_create_verification_codes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS verification_codes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSINGED NOT NULL,
     code VARCHAR(6) NOT NULL,
     type ENUM('email_verification', 'password_reset') NOT NULL,
     expires_at TIMESTAMP NOT NULL,
